bac150fd3c0a735bc9c55b5a9c761f06e065914d
Yuliyanti Dewi Lestari <yuliyantidewilestari@gmail.com>
2025-05-25 01:00:51 +0700

    edit folder app

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, UploadFile, Depends
+
+'''from fastapi import FastAPI, UploadFile, Depends
 from utils import predictor
 from app import inference
 from tensorflow.keras.models import load_model
@@ -13,3 +14,17 @@ def initialize_model():
         model = load_model('models/model_wasting.keras')
         return model
 
+'''
+from predictor import StuntingPredictor, WastingPredictor
+
+stunting_pred = StuntingPredictor('models/model_stunting.h5', 'stunting-predictior-ml/scaler.pkl')
+wasting_pred = WastingPredictor('models/model_wasting.keras', 'stunting-predictior-ml/scaler_wt.pkl')
+
+input_stunting = [0, 10, 70, 8]
+input_wasting = [0, 10, 70, 8]
+
+hasil_stunting = stunting_pred.predict(input_stunting)
+hasil_wasting = wasting_pred.predict(input_wasting)
+
+print("Hasil prediksi stunting:", hasil_stunting)
+print("Hasil prediksi wasting:", hasil_wasting)
